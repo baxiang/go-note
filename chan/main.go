74d29b087b037ac70497d4bf3fc6acd3d2e30dbd
baxiang <baxiang@roobo.com>
2020-01-16 14:17:33 +0800

    vendor

--- chan/main.go
+++ chan/main.go
@@ -1,5 +1,15 @@
 package main
 
+import "fmt"
+
 func main() {
-	$END$
+	//done := make(chan int)
+
+	go func(){
+		fmt.Println("你好, 世界")
+		//done<-1
+	}()
+
+	//<-done
 }
+
--- compare/main.go
+++ compare/main.go
@@ -1,5 +1,123 @@
 package main
 
-func main() {
-	$END$
+import (
+
+	"fmt"
+
+	"sort"
+
+	"strings"
+
+)
+
+
+type StuScore struct {
+
+	name  string
+
+	score int
+
 }
+
+
+type StuScores []StuScore
+
+
+func (s StuScores) Len() int {
+
+	return len(s)
+
+}
+
+
+func (s StuScores) Less(x, y int) bool {
+
+	return s[x].score < s[y].score
+
+}
+
+func (s StuScores) Swap(x, y int) {
+
+	s[x], s[y] = s[y], s[x]
+
+}
+
+
+type SortByName struct {
+
+	StuScores
+
+}
+
+
+func (s SortByName) Less(x, y int) bool {
+
+	if strings.Compare(s.StuScores[x].name, s.StuScores[y].name) < 1 {
+
+		return true
+
+	}
+
+	return false
+
+}
+
+
+type SortByAgeDESC struct {
+
+	StuScores
+
+}
+
+
+func (s SortByAgeDESC) Less(x, y int) bool {
+
+	return s.StuScores[x].score > s.StuScores[y].score
+
+}
+
+
+func main() {
+
+	stus := StuScores{{"name2", 95}, {"name1", 75}, {"name5", 86}, {"name4", 60}, {"name3", 100}}
+
+	fmt.Println("排序前------------------")
+
+	for _, v := range stus {
+
+		fmt.Println(v.name, ":", v.score)
+
+	}
+
+	fmt.Println("按照分数升序排序------------------")
+
+	sort.Sort(stus)
+
+	for _, v := range stus {
+
+		fmt.Println(v.name, ":", v.score)
+
+	}
+
+	fmt.Println("按照姓名排序------------------")
+
+	sort.Sort(SortByName{stus})
+
+	for _, v := range stus {
+
+		fmt.Println(v.name, ":", v.score)
+
+	}
+
+	fmt.Println("按照分数降序排序------------------")
+
+	sort.Sort(SortByAgeDESC{stus})
+
+	for _, v := range stus {
+
+		fmt.Println(v.name, ":", v.score)
+
+	}
+
+}
+
