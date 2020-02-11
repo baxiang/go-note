8e2fc226bdee27b4fdadbc2386ab147e5fa27218
baxiang <baxiang@roobo.com>
2020-02-11 19:49:52 +0800

    function

--- function/firstclass.go
+++ function/firstclass.go
@@ -1,5 +1,16 @@
 package main
 
+import "fmt"
+
+func square(n int)int{
+	return n*n
+}
+func negative(n int)int{
+	return -n
+}
 func main() {
-	$END$
+	f :=square
+	fmt.Println(f(5))
+	f =negative
+	fmt.Println(f(5))
 }
--- function/operate.go
+++ function/operate.go
@@ -1,5 +1,28 @@
 package main
 
+import (
+	"errors"
+	"fmt"
+)
+
+func operation(a,b int,symbol string)(result int,err error){
+	switch symbol {
+	case "+":
+		return a+b,nil
+	case "-":
+		return a-b,nil
+	case "*":
+		return a*b,nil
+	case "/":
+		if b==0 {
+			return 0,errors.New("division by zero")
+		}
+		return a/b,nil
+	}
+	return 0,errors.New("unsupported symbol:"+symbol)
+}
+
 func main() {
-	$END$
+	r,_:=operation(2,3,"+")
+	fmt.Println(r)
 }
