d30a07284d8265a08dcd23b82fe453c530fe1398
baxiang <baxiang@roobo.com>
2020-03-31 17:37:51 +0800

    mutilefunc

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-#Go语言代码笔记
+# Go语言代码笔记
 网址：https://www.yuque.com/baxiang/go
 
 |文章|代码地址|笔记地址|
---
+++ function/mutilfunc.go
@@ -0,0 +1,26 @@
+package main
+
+import "fmt"
+
+func min(a...int) int{
+	if len(a)==0 {
+		return 0
+
+	}
+	min := a[0]
+	for _,v :=range a{
+		if min >v {
+			min = v
+		}
+	}
+	return min
+}
+func main() {
+	n := min(17,12,5,9,8)
+	fmt.Println(n)
+	arr := []int {6,27,5,67,18}
+	r := min(arr...)
+	fmt.Println(r)
+	m := min(arr[:3]...)
+	fmt.Println(m)
+}
