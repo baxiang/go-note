47d182cf621d30fb88713ca6f4e7d472df8338d4
baxiang <baxiang@roobo.com>
2020-04-01 22:52:20 +0800

    goto

--- flowcontrol/goto.go
+++ flowcontrol/goto.go
@@ -4,11 +4,23 @@ import "fmt"
 
 func main() {
 	for i := 0; i <= 2; i++ {
-		for j := 1; j <= 3; j++ {
-			if j == 2 {
-				break
+			for j := 1; j <= 3; j++ {
+				if j == 2 {
+					goto flag
+				}
+				fmt.Printf("%d+%d=%d\n", i, j, i+j)
 			}
-			fmt.Printf("%d+%d=%d\n", i, j, i+j)
 		}
-	}
+flag :
+	foo()
+}
+
+func foo(){
+	var s = "hello world"
+	goto flag
+
+	fmt.Println(s)
+flag:
+	s = "hello golang"
+	fmt.Println(s)
 }
