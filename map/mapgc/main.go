eb1e54e6721ba711c5c06cb3254ac91dd683cead
baxiang <baxiang@roobo.com>
2020-04-08 12:32:38 +0800

    map gc 验证

--- map/mapgc/main.go
+++ map/mapgc/main.go
@@ -1,5 +1,31 @@
 package main
 
+import (
+	"log"
+	"runtime"
+)
+
+func printMemStats(mag string) {
+	var m runtime.MemStats
+	runtime.ReadMemStats(&m)
+	log.Printf("%v：分配的内存 = %vKB, GC的次数 = %v\n", mag, m.Alloc/1024, m.NumGC)
+}
+
+// go version go1.12.5 linux/amd64 不需要
 func main() {
-	$END$
+	m := make(map[int]int)
+	for i := 0; i < 10000; i++ {
+		m[i] = i
+	}
+	runtime.GC()
+	printMemStats("add map")
+	for i := 0; i < 10000; i++ {
+		delete(m, i)
+	}
+	runtime.GC()
+	printMemStats("clear map")
+	m = nil
+	runtime.GC()
+	printMemStats("set nil")
+
 }
