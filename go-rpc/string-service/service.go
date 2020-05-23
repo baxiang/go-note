df6b3660d0fd67f229bc44d226769f3f4be760fe
baxiang <yangyucug@gmail.com>
2020-05-24 00:17:15 +0800

    rpc

---
+++ go-rpc/string-service/service.go
@@ -0,0 +1,52 @@
+package service
+
+import (
+	"context"
+	"errors"
+	"github.com/baxiang/go-note/go-rpc/pb"
+	"strings"
+)
+var (
+	ErrMaxSize = errors.New("maximum size of 1024 bytes exceeded")
+
+	ErrStrValue = errors.New("maximum size of 1024 bytes exceeded")
+)
+
+const (
+	StrMaxSize = 1024
+)
+type StringService struct{}
+
+
+func (s *StringService) Concat(ctx context.Context, req *pb.StringRequest) (*pb.StringResponse, error) {
+	if len(req.A)+len(req.B) > StrMaxSize {
+		response := pb.StringResponse{Ret: ""}
+		return &response, nil
+	}
+	response := pb.StringResponse{Ret: req.A + req.B}
+	return &response, nil
+}
+
+
+func (s *StringService) Diff(ctx context.Context, req *pb.StringRequest) (*pb.StringResponse, error) {
+	if len(req.A) < 1 || len(req.B) < 1 {
+		response := pb.StringResponse{Ret: ""}
+		return &response, nil
+	}
+	res := ""
+	if len(req.A) >= len(req.B) {
+		for _, char := range req.B {
+			if strings.Contains(req.A, string(char)) {
+				res = res + string(char)
+			}
+		}
+	} else {
+		for _, char := range req.A {
+			if strings.Contains(req.B, string(char)) {
+				res = res + string(char)
+			}
+		}
+	}
+	response := pb.StringResponse{Ret: res}
+	return &response, nil
+}
