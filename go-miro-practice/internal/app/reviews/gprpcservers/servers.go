f452055e1271a97c3a798df5b609d0db7cd1b6e2
baxiang <yangyucug@gmail.com>
2020-05-28 21:46:14 +0800

    delete micro

--- go-miro-practice/internal/app/reviews/gprpcservers/servers.go
+++
@@ -1,5 +0,0 @@
-package gprpcservers
-
-
-
-
--- go-miro-practice/internal/pkg/models/review.go
+++
@@ -1,9 +0,0 @@
-package models
-
-import "time"
-type Review struct {
-	ID          uint64    `json:"id"`
-	ProductID   uint64    `json:"product_id"`
-	Message     string    `json:"message"`
-	CreatedTime time.Time `json:"created_time"`
-}
